fae345f51684872aadcd7e0da1d155de0b18c74a
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-10-18 21:46:32 +0300

    Initial commit

---
+++ treasure.py
@@ -0,0 +1,4 @@
+print("Welcome to treasure Island")
+print("Your Mission is to find the treasure!")
+
+direction = input("You're at a cross road. Where do you want to go? Type 'Left' or 'Right' \n")
