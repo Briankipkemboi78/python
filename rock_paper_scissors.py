e045b792d8db430e6f63ea4a1da89af3919ce5d4
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-10-19 10:11:36 +0300

    Refactored the code

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,11 +5,12 @@ options = ["Rock", "Paper", "Scissors"]
 choices = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
 
 computer_choice = random.randint(0, 2)
-
 print(f"You chose: {options[choices]}")
 print(f"Computer chose: {options[computer_choice]}")
 
-if choices == computer_choice:
+if choices >= 3:
+    print("You picked and invalid number. You lose")
+elif choices == computer_choice:
     print("It's a draw")
 elif choices == 0 and computer_choice == 1:
     print("You Lose")
@@ -23,5 +24,3 @@ elif choices == 2 and computer_choice == 0:
     print("You Lose")
 elif choices == 2 and computer_choice == 1:
     print("You Win")
-else:
-    print("You lose!")
