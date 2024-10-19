049a8998e781a9dd526aa1d8be6c281b7a4fffc0
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-10-19 09:36:08 +0300

    Rock paper Scissors

---
+++ rock_paper_scissors.py
@@ -0,0 +1,25 @@
+import random
+
+options = ["Rock", "Paper", "Scissors"]
+
+choices = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors\n"))
+
+computer_choice = random.randint(0,2)
+
+print(f"You chose: {options[choices]}")
+print(f"Computer chose: {options[computer_choice]}")
+
+if choices == computer_choice:
+    print("It's a draw")
+elif choices == 0 and computer_choice == 1:
+    print("You Lose")
+elif choices  == 0 and computer_choice == 2:
+    print("You Win")
+elif choices  == 1 and computer_choice == 0:
+    print("You Win")
+elif choices  == 1 and computer_choice == 2:
+    print("You Lose")
+elif choices  == 2 and computer_choice == 0:
+    print("You Lose")
+elif choices  == 2 and computer_choice == 1:
+    print("You Win")
