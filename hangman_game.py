2e99edcabb1ffad09576c05e775308916bcb4ffe
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-10-21 10:18:17 +0300

    Improved the functionality.

--- hangman_game.py
+++ hangman_game.py
@@ -2,22 +2,40 @@ import random
 
 word_list = ["baboon", "mouse", "church"]
 
+lives = 6
 chosen_word = random.choice(word_list)
 
-print(chosen_word)
 placeholder = ""
 word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
 print(f"Guess the word: {placeholder}")
 
-user_guess = input("Please guess a letter \n").lower()
+game_over = False
+correct_letters = []
 
-display = ""
+while not game_over:
+    user_guess = input("Please guess a letter \n").lower()
 
-for letter in chosen_word:
-    if letter == user_guess:
-        display += letter
-    else:
-        display += "_"
-print(display)
+    display = ""
+
+    for letter in chosen_word:
+        if letter == user_guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+
+    if user_guess not in chosen_word:
+        lives -= 1
+        print(f"You are wrong: You have {lives} lives remaining")
+        if lives == 0:
+            game_over = True
+            print("You lose! Game over")
+
+    if "_" not in display:
+        game_over = True
+        print("You win")
