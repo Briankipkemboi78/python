c99bdd68290edae007a9ab533579f2457e5525f5
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-10-21 14:39:38 +0300

    Initial commit

---
+++ Ceaser Cipher/README.md
@@ -0,0 +1,2 @@
+### Ceaser Cypher
+This is to practice the use of function together with arguments.
---
+++ Ceaser Cipher/ceaser_cypher.py
@@ -0,0 +1,42 @@
+print("Welcome to Ceaser Cypher!")
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message: \n").lower()
+shift = int(input("Enter the shift number: \n"))
+
+
+def encrypt(text, shift):
+    encrypted_text = ""
+    for i in text:
+        if i in alphabet:
+            position = alphabet.index(i)
+            new_position = (position + shift) % 26
+            encrypted_text += alphabet[new_position]
+        else:
+            encrypted_text += i
+    print(f"Your encrypted message is: {encrypted_text}")
+
+
+def decrypt(text, shift):
+    decrypted_text = ""
+    for i in text:
+        if i in alphabet:
+            position = alphabet.index(i)
+            new_position = (position - shift) % 26
+            decrypted_text += alphabet[new_position]
+        else:
+            decrypted_text += i
+    print(f"Your decrypted message is: {decrypted_text}")
+
+
+if direction == "encode":
+    encrypt(text, shift)
+elif direction == "decode":
+    decrypt(text,shift)
+else:
+    print("You have entered the wrong key!")
