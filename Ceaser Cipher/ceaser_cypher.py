0fdf46ff2a0e0680d1038199f729f38b14e60246
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-10-21 14:57:38 +0300

    Added more functionality

--- Ceaser Cipher/ceaser_cypher.py
+++ Ceaser Cipher/ceaser_cypher.py
@@ -2,8 +2,7 @@ print("Welcome to Ceaser Cypher!")
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z']
+            'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z']
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message: \n").lower()
