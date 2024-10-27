81b77b03775aba900d9c98e1fc997255e15c5aea
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-10-27 11:27:48 +0300

    Improving the functionality of the game

--- namespaces/number_guessing.py
+++ namespaces/number_guessing.py
@@ -1,5 +1,6 @@
 import random
 
+
 def number_guessing_game():
     print("Welcome to the Number Guessing Game!")
     print("I'm thinking of a number between 1 an 100")
@@ -8,7 +9,6 @@ def number_guessing_game():
     HARD = 5
 
     guess_number = random.randint(1, 100)
-
 
     is_game_over = False
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
@@ -27,7 +27,6 @@ def number_guessing_game():
             print(f"Congrats! You guessed the number {guess_number} correctly.")
             is_game_over = True
 
-
         attempts -= 1
 
         if attempts == 0:
@@ -35,5 +34,6 @@ def number_guessing_game():
             is_game_over = True
         else:
             print("Guess Again")
+
 
 number_guessing_game()
