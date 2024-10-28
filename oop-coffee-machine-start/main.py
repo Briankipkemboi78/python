bfa0047e5c4691be8344fff39783a91ac9f72bef
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-10-28 14:33:30 +0300

    Implemented use of OOP

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -1,3 +1,30 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu = Menu()
+coffe_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+
+def make_coffee(drink_name, order_ingredients):
+    for item in order_ingredients:
+        MenuItem[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name}. Enjoy!")
+
+
+is_on = True
+while is_on:
+    choice = input(
+        "What would you like? (espresso/latte/cappuccino) \n Or Type 'report' to get the resource report: \n").lower()
+
+    if choice == "off":
+        is_on = False
+
+    elif choice == "report":
+        coffe_maker.report()
+
+    else:
+        drink = menu.find_drink(choice)
+        if drink and coffe_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffe_maker.make_coffee(drink)
