db769dfc2a0e7908b0dc7bd047f0577318c3397f
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-10-29 12:04:16 +0300

    Completing the game

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -12,5 +12,9 @@ for item in question_data:
 
 quiz = QuizBrain(question_bank)
 
+
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You've completed the Quizzes!")
+print(f"Yo final score was: {quiz.score}/{quiz.question_number}")
