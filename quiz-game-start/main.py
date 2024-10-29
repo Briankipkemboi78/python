0efc22a616c91839b67d17650f1fd5f74d6ddcc3
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-10-29 11:27:06 +0300

    Adding fetching questions capability

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,6 +1,7 @@
 from question_model import Question
-
 from data import question_data
+from quiz_brain import QuizBrain
+
 
 question_bank = []
 for item in question_data:
@@ -8,3 +9,8 @@ for item in question_data:
     question_answer = item["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
