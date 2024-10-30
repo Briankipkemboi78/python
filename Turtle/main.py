e392c397a119995de8171833fb20b7b5e11e678b
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-10-30 11:22:24 +0300

    Changing data bank

---
+++ Turtle/main.py
@@ -0,0 +1,18 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+
+num_sides = 4
+while num_sides <= 10:
+
+    angle = 360 / num_sides
+
+    for _ in range(num_sides):
+        timmy.forward(100)
+        timmy.right(angle)
+        timmy.color("cyan")
+
+    num_sides += 1
+
+screen = Screen()
+screen.exitonclick()
