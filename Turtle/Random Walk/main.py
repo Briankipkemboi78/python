87cb920f532ad1c8d10401a0ab38b5db66118cae
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-01 08:22:47 +0300

    Replacing list with randomly generated color

--- Turtle/Random Walk/main.py
+++ Turtle/Random Walk/main.py
@@ -1,27 +1,27 @@
 import random
+import turtle
 from turtle import Turtle, Screen
 
 timmy = Turtle()
+turtle.colormode(255)
+timmy.speed("fastest")
 
 # List of colors
-colors = [
-    "red", "blue", "green", "orange", "purple", "yellow",
-    "brown", "pink", "cyan", "magenta", "lime", "maroon",
-    "navy", "olive", "teal", "coral", "gold", "turquoise"
-]
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
 
+    return (r,g,b)
+
+directions = [0, 90, 180, 270]
 # Create a loop for the turtle's random movement with color changes
 for _ in range(100):
-    timmy.pensize(5)
-    timmy.color(random.choice(colors))  # Choose a random color from the list
+    timmy.pensize(8)
+    timmy.color(random_color())  # Choose a random color from the list
     timmy.forward(30)
-    angle = random.choice([0, 90, 180, 270])  # Choose a random 90-degree angle
+    angle = random.choice(directions)  # Choose a random 90-degree angle
     timmy.right(angle)
-
-
-
-
-
 
 
 
