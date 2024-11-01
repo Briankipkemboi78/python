5b62248bdab4dfa24649901437670e215416a7e5
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-01 15:33:51 +0300

    Formatting to pep8 guidelines

--- Turtle/spirograph.py
+++ Turtle/spirograph.py
@@ -6,6 +6,7 @@ tim = Turtle()
 tim.speed("fastest")
 turtle.colormode(255)
 
+
 def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
@@ -23,8 +24,5 @@ for _ in range(circles):
     current_heading = tim.heading()
     tim.setheading(current_heading + 5)
 
-
-
 screen = Screen()
 screen.exitonclick()
-
