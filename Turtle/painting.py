5dd5a9d21c5661c5dd908b0b83d6fca66e795872
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-02 08:44:31 +0300

    Improving the user experience

--- Turtle/painting.py
+++ Turtle/painting.py
@@ -12,11 +12,28 @@ color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), 
 tim = turtle_module.Turtle()
 turtle_module.colormode(255)
 tim.shape("circle")
+tim.speed("fastest")
 
-for _ in range(10):
-    tim.penup()
+
+tim.penup()
+tim.hideturtle()
+tim.setheading(225)
+tim.forward(300)
+tim.setheading(0)
+
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
     tim.dot(20, random.choice(color_list))
     tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
 
 
 
