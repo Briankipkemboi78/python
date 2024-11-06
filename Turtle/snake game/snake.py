7423481fafbddb45d135350865174655cfa229d6
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-06 11:22:53 +0300

    Implementing direction movement using keystrokes

--- Turtle/snake game/snake.py
+++ Turtle/snake game/snake.py
@@ -1,7 +1,12 @@
+import turtle
 from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake:
@@ -9,6 +14,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
         self.move()
 
     def create_snake(self):
@@ -25,3 +31,19 @@ class Snake:
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.segments[0].forward(MOVE_DISTANCE)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
