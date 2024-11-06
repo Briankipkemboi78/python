cee332996f1963cb3af7d64c814218ae9f7f97b2
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-06 08:49:53 +0300

    Moving the segments all at once

--- Turtle/snake game/snake.py
+++ Turtle/snake game/snake.py
@@ -1,3 +1,4 @@
+import time
 import turtle
 from turtle import Screen, Turtle
 
@@ -5,16 +6,30 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
 starting_position = [(-40, 0), (-20, 0), (0, 0)]
 
 # Create turtles and position them on the screen
-all_turtles = []
+segments = []
 for position in starting_position:
     new_turtle = Turtle(shape="square")
     new_turtle.color("white")  # Set color using hex code
     new_turtle.penup()
     new_turtle.goto(position)
-    all_turtles.append(new_turtle)
+    segments.append(new_turtle)
+
+screen.update()
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(1)
+    for seg in segments:
+        seg.forward(20)
+
+
+
 
 screen.exitonclick()
