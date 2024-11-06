7e37b2802bf92feddfc2c52a37043b536f149331
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-06 10:51:42 +0300

    Creating objects for cleaner structure

--- Turtle/snake game/main.py
+++ Turtle/snake game/main.py
@@ -1,6 +1,7 @@
 import time
 import turtle
 from turtle import Screen, Turtle
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,18 +9,8 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
 
-# Create turtles and position them on the screen
-segments = []
-for position in starting_position:
-    new_turtle = Turtle(shape="square")
-    new_turtle.color("white")  # Set color using hex code
-    new_turtle.penup()
-    new_turtle.goto(position)
-    segments.append(new_turtle)
-
-screen.update()
+snake = Snake()
 
 
 game_is_on = True
@@ -27,11 +18,9 @@ while game_is_on:
     screen.update()
     time.sleep(1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x , new_y)
-    segments[0].forward(20)
+    snake.move()
+
+
 
 
 
