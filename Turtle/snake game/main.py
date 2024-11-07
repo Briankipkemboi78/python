3aa950672c141606f9382b84b2020e38f3b09929
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-07 08:03:15 +0300

    Concluding the project

--- Turtle/snake game/main.py
+++ Turtle/snake game/main.py
@@ -33,5 +33,11 @@ while game_is_on:
     #Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 screen.exitonclick()
--- Turtle/snake game/snake.py
+++ Turtle/snake game/snake.py
@@ -18,12 +18,19 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_turtle = Turtle(shape="square")
-            new_turtle.color("white")  # Set color using hex code
-            new_turtle.penup()
-            new_turtle.goto(position)
-            new_turtle.turtlesize(stretch_wid=0.5, stretch_len=0.5)
-            self.segments.append(new_turtle)
+            self.add_segment(position)
+
+
+    def add_segment(self,position):
+        new_turtle = Turtle(shape="square")
+        new_turtle.color("white")  # Set color using hex code
+        new_turtle.penup()
+        new_turtle.goto(position)
+        new_turtle.turtlesize(stretch_wid=0.5, stretch_len=0.5)
+        self.segments.append(new_turtle)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self) -> object:
         for seg_num in range(len(self.segments) - 1, 0, -1):
