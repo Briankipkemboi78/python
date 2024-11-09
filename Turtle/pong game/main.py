15317494776777cd9caa7430b849cc7030ac76c5
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-09 10:56:53 +0300

    Completing the game

--- Turtle/pong game/main.py
+++ Turtle/pong game/main.py
@@ -1,9 +1,13 @@
+import time
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
 
 
 LEFT_POSITION = (-350, 0)
 RIGHT_POSITION = (350, 0)
+BALL_POSITION = (350, 350)
 
 
 screen = Screen()
@@ -15,6 +19,8 @@ screen.tracer(0)
 # Creating a paddle
 r_paddle = Paddle(RIGHT_POSITION)
 l_paddle = Paddle(LEFT_POSITION)
+ball = Ball(BALL_POSITION)
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -25,17 +31,26 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
 
+    # Detect with collision
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
+    # Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 340 or ball.distance(l_paddle) < 50 and ball.xcor() < -340:
+        ball.bounce_x()
 
+    # Detect ball missing the r_paddle
+    if ball.xcor() > 370:
+        ball.reset_position()
+        scoreboard.l_point()
 
-
-
-
-
-
-
-
+    # Detect ball missing the l_paddle
+    if ball.xcor() < -370:
+        ball.reset_position()
+        scoreboard.r_point()
 
 screen.exitonclick()
