8ecb2ca18eec0ed90b3bf9b3394bd0449624c4e1
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-09 09:11:26 +0300

    Creating left paddle

--- Turtle/pong game/main.py
+++ Turtle/pong game/main.py
@@ -1,17 +1,31 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 
+
+LEFT_POSITION = (-350, 0)
+RIGHT_POSITION = (350, 0)
+
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+screen.tracer(0)
 
 # Creating a paddle
-r_paddle = Paddle()
+r_paddle = Paddle(RIGHT_POSITION)
+l_paddle = Paddle(LEFT_POSITION)
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
+
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
 
 
 
--- Turtle/pong game/paddle.py
+++ Turtle/pong game/paddle.py
@@ -1,16 +1,14 @@
 from turtle import Turtle
 
 
-
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, position):
         super().__init__()
         self.color("white")
         self.shape("square")
         self.penup()
         self.shapesize(stretch_wid=5, stretch_len=1)
-        self.goto(350, 0)
-
+        self.goto(position)
 
     def go_up(self):
         new_y = self.ycor() + 20
@@ -19,4 +17,3 @@ class Paddle(Turtle):
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
-
