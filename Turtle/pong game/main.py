83afca867077c60bf0be98861e735ef8caeb7320
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-09 08:54:23 +0300

    Creating right paddle

--- Turtle/pong game/main.py
+++ Turtle/pong game/main.py
@@ -1,9 +1,19 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+
+# Creating a paddle
+r_paddle = Paddle()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+
+
 
 
 
