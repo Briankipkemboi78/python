777839ad53d7fc68f2f7b808caa1f2fa535f7772
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-11 15:05:06 +0300

    Creating scoreboard

--- Turtle/turtle crossing/scoreboard.py
+++ Turtle/turtle crossing/scoreboard.py
@@ -2,7 +2,23 @@ from turtle import Turtle
 
 
 
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 15, "normal")
 
 class Scoreboard(Turtle):
-    pass
+    def __init__(self):
+        super().__init__()
+
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-370, 260)
+        self.update_scoreboard()
+
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
