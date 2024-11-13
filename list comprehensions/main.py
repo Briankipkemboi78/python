6d4e867611c2dc80387b0bddba438bc80637c691
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-13 14:58:13 +0300

    Improving the output

--- list comprehensions/main.py
+++ list comprehensions/main.py
@@ -2,15 +2,14 @@ import pandas as pd
 
 data = pd.read_csv('nato_phonetic_alphabet.csv')
 
-
-
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 user_name = input("Please enter your name: \n").upper()
 splitted_name = list(user_name)
 
 
-your_name = {letter: nato_dict[letter] for letter in splitted_name if letter in nato_dict}
+# your_name = {letter: nato_dict[letter] for letter in splitted_name if letter in nato_dict} #This gets the letter and its code
+your_name = [nato_dict[letter] for letter in splitted_name if letter in nato_dict]
 
 print(your_name)
 
