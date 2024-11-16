f8aedaa9c3f82e224132b54366b55d900596d4cf
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-16 20:48:07 +0300

    Adding buttons and entry field

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -7,7 +7,7 @@ GREENISH = "#243642"
 GREEN = "#9bdeac"
 SAGE = "#629584"
 TEAL = "#387478"
-YELLOW = "#f7f5dd"
+GRADIENT = "#E2F1E7"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -37,11 +37,15 @@ text_label = Label(text="TIMER", font=(FONT_NAME, 40, "bold"), bg=GREENISH, fg=T
 text_label.grid(column=1, row=0)
 
 # Buttons
-start_button = Button(text="Start", bg=SAGE, fg=TEAL)
+start_button = Button(text="Start", bg=SAGE, fg=GRADIENT, font=(FONT_NAME, 12))
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", bg=SAGE, fg=TEAL)
+reset_button = Button(text="Reset", bg=SAGE, fg=GRADIENT, font=(FONT_NAME, 12))
 reset_button.grid(column=2, row=2)
+
+# Entry
+check_entry = Entry(width=10, bg=SAGE, fg=GRADIENT)
+check_entry.grid(column=1,row=3)
 
 
 
