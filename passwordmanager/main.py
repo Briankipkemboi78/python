dd1de70b7a0c62e4e5a0ef950f33d8ccdac1ac00
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-20 18:10:53 +0300

    Setting up the application

---
+++ passwordmanager/main.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+paddlock = PhotoImage(file='logo.png')
+canvas.create_image(100,100, image=paddlock)
+canvas.grid(column=1, row=1)
+
+
+
+
+
+
+
+
+window.mainloop()
