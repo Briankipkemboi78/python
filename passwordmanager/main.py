3b653933675fc04f011d00c1c2430f78896d91be
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-21 08:05:04 +0300

    Adding functionalities

--- passwordmanager/main.py
+++ passwordmanager/main.py
@@ -1,8 +1,5 @@
 from tkinter import *
 
-
-
-
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -14,7 +11,7 @@ window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=200)
 logo_image = PhotoImage(file='logo.png')
-canvas.create_image(100,100, image=logo_image)
+canvas.create_image(100, 100, image=logo_image)
 canvas.grid(column=1, row=0)
 
 # Labels
@@ -27,11 +24,23 @@ user_label.grid(column=0, row=2)
 password_label = Label(text="Password:", font=('Arial', 14))
 password_label.grid(column=0, row=3)
 
+#Input
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=1, columnspan=2)
 
+#-----Email/Username-
+website_entry = Entry(width=35)
+website_entry.grid(column=1, row=2, columnspan=2)
 
+#----Password-
+website_entry = Entry(width=21)
+website_entry.grid(column=1, row=3)
 
+#Buttons
+generate_button = Button(text="Generate Password", font=('Arial', 15, 'bold'))
+generate_button.grid(column=2, row=3)
 
-
-
+add_button = Button(text="Add", font=('Arial', 15, 'bold'))
+add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
