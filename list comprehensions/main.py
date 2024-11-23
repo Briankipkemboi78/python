b18f7f3bfa694dd3e1768fffe821ffbceac3f054
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-11-23 10:21:47 +0300

    Error handling

--- list comprehensions/main.py
+++ list comprehensions/main.py
@@ -4,12 +4,16 @@ data = pd.read_csv('nato_phonetic_alphabet.csv')
 
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter a word:").upper()
 
-try:
-    output_list = [nato_dict[letter] for letter in word]
-except KeyError:
-    print("Sorry only letters allowed")
+def generate_phonetic():
+    word = input("Enter a word:").upper()
+    try:
+        output_list = [nato_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry only letters allowed")
+        generate_phonetic()
+    else:
+        print(output_list)
 
-print(output_list)
 
+generate_phonetic()
