f0f65ed3b526930ff3fd078d3143b419ec7638e5
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-12-01 10:29:14 +0300

    Improving functionality

--- brithday tracker/main.py
+++ brithday tracker/main.py
@@ -3,17 +3,34 @@ import random
 import datetime as dt
 import smtplib
 
-now = dt.datetime.now()
-today_month = now.month
-today_day = now.day
+USER_MAIL = "briankipkemboi47@gmail.com"
+PASSWORD = 'mnlfzmghyifmqrmc'
 
-today = (today_month, today_day)
-print(today)
+
+now = dt.datetime.now()
+today = (now.month, now.day)
 
 # Reading the file
 data = pd.read_csv('birthdays.csv')
 
-#birthday_dict = {birthday_month: birthday_day for (index, data_roe) in data.iterrows()}
+birthday_dict = {(data_row['month'], data_row['day']): data_row for (index, data_row) in data.iterrows()}
+
+# Check if the birthday exists
+if today in birthday_dict:
+    birthday_person = birthday_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        new_message = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=USER_MAIL, password=PASSWORD)
+        connection.sendmail(
+            from_addr=USER_MAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject: Happy Birthday!\n\n {new_message}"
+        )
 
 
 
