390e292786a984c6d5ae4fea5b410a4876b756ab
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-12-01 10:33:13 +0300

    Improving functionality

--- brithday tracker/main.py
+++ brithday tracker/main.py
@@ -21,7 +21,7 @@ if today in birthday_dict:
     file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
-        new_message = contents.replace("[NAME]", birthday_person["name"])
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
@@ -29,7 +29,7 @@ if today in birthday_dict:
         connection.sendmail(
             from_addr=USER_MAIL,
             to_addrs=birthday_person["email"],
-            msg=f"Subject: Happy Birthday!\n\n {new_message}"
+            msg=f"Subject: Happy Birthday!\n\n {contents}"
         )
 
 
