8f48c032d99643b30989a94e4e8e034278af9496
Briankipkemboi78 <briankipkemboi47@gmail.com>
2024-12-01 10:34:12 +0300

    Cleaning up the code

--- brithday tracker/main.py
+++ brithday tracker/main.py
@@ -31,15 +31,3 @@ if today in birthday_dict:
             to_addrs=birthday_person["email"],
             msg=f"Subject: Happy Birthday!\n\n {contents}"
         )
-
-
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
